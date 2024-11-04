98225a42b92ef5cbf51fe03273d01d1ca40ebec3
zé <dudd.vitor@gmail.com>
2024-11-04 16:58:59 -0300

    configurando dependências

--- src/main/java/ifsp/vitaesangue/VitaesangueApplication.java
+++ src/main/java/ifsp/vitaesangue/VitaesangueApplication.java
@@ -1,4 +1,4 @@
-package ifsp.vitaesangue.vitaesangue;
+package ifsp.vitaesangue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
