151ef4b1765c9e92758260d3bacbc7ff69e59a4a
José Vitor Sargentini <dudd.vitor@gmail.com>
2024-11-04 09:40:38 -0300

    Initial push

---
+++ src/test/java/ifsp/vitaesangue/vitaesangue/VitaesangueApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsp.vitaesangue.vitaesangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaesangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
