8740302e2520d2ce8258bb439085782931845c72
petersonianif <peterson.ferreira@aluno.ifsp.edu.br>
2024-11-17 12:06:23 -0300

    feat: create usuario model, repository and controller

---
+++ src/main/java/ifsp/vitaesangue/controller/UsuarioController.java
@@ -0,0 +1,74 @@
+package ifsp.vitaesangue.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ifsp.vitaesangue.model.Estabelecimento;
+import ifsp.vitaesangue.model.Perfil;
+import ifsp.vitaesangue.model.Usuario;
+import ifsp.vitaesangue.repository.EstabelecimentoRepository;
+import ifsp.vitaesangue.repository.PerfilRepository;
+import ifsp.vitaesangue.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private PerfilRepository perfilRepository;
+	
+	@Autowired
+	private EstabelecimentoRepository estabelecimentoRepository;
+	
+	@GetMapping
+	public List<Usuario> getAll() {
+		return usuarioRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable("id") Long id) { 
+		return usuarioRepository.findById(id) .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public Usuario create(@RequestBody Usuario usuario) {
+		Estabelecimento estabelecimento = estabelecimentoRepository.findById(usuario.getEstabelecimento().getId())
+                .orElseThrow(() -> new RuntimeException("Estabelecimento não encontrado"));
+	
+		usuario.setEstabelecimento(estabelecimento);
+		
+		Perfil perfil = perfilRepository.findById(usuario.getPerfil().getPerfilId())
+                .orElseThrow(() -> new RuntimeException("Perfil não encontrado"));
+	
+		usuario.setPerfil(perfil);
+		
+		usuarioRepository.save(usuario);
+		return usuario;
+	}
+	
+	@PutMapping()
+	public Usuario edit(@RequestBody Usuario usuario) {
+	    
+	    return usuarioRepository.findById(usuario.getUsuarioId())
+	      .map(usuarioEntity -> {
+	    	 usuarioEntity.setNome(usuario.getNome());
+	        return usuarioRepository.save(usuarioEntity);
+	      })
+	      .orElseGet(() -> {
+	        return usuarioRepository.save(usuario);
+	      });
+	 }
+
+}
---
+++ src/main/java/ifsp/vitaesangue/model/Usuario.java
@@ -0,0 +1,133 @@
+package ifsp.vitaesangue.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usuario_id")
+    private Long usuarioId;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+
+    @Column(name = "ddd")
+    private Integer ddd;
+
+    @Column(name = "telefone")
+    private Integer telefone;
+
+    @Column(name = "whatsapp")
+    private Boolean whatsapp;
+
+    @Embedded
+    private Endereco endereco;
+
+    @ManyToOne(targetEntity = Estabelecimento.class)
+    @JoinColumn(name = "estabelecimento_id")
+    private Estabelecimento estabelecimento;
+
+    @ManyToOne(targetEntity = Perfil.class)
+    @JoinColumn(name = "perfil_id")
+    private Perfil perfil;
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Integer getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(Integer ddd) {
+        this.ddd = ddd;
+    }
+
+    public Integer getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Integer telefone) {
+        this.telefone = telefone;
+    }
+
+    public Boolean getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(Boolean whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+
+    public Estabelecimento getEstabelecimento() {
+        return estabelecimento;
+    }
+
+    public void setEstabelecimento(Estabelecimento estabelecimento) {
+        this.estabelecimento = estabelecimento;
+    }
+
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+}
