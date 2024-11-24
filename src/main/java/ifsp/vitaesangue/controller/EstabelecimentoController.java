31b5de5ff39f244e21fa0feab664198a697ec9ba
gabriel giacometti <gabrielhdfg460@gmail.com>
2024-11-23 22:03:48 -0300

    feat: add delete estabelecimento

--- src/main/java/ifsp/vitaesangue/controller/EstabelecimentoController.java
+++ src/main/java/ifsp/vitaesangue/controller/EstabelecimentoController.java
@@ -4,13 +4,18 @@ package ifsp.vitaesangue.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ifsp.vitaesangue.model.Estabelecimento;
+import ifsp.vitaesangue.model.Perfil;
 import ifsp.vitaesangue.repository.EstabelecimentoRepository;
 
 
@@ -30,8 +35,15 @@ public class EstabelecimentoController {
 	@PostMapping
 	public Estabelecimento create(@RequestBody Estabelecimento estabelecimento) {
 		
-		estabelecimento.setAtivo(false);
 		estabelecimentoRepository.save(estabelecimento);
 		return estabelecimento;
 	}
+	
+	@DeleteMapping("/{id}")
+	public void  delete(@PathVariable("id") Long id) {
+		
+		estabelecimentoRepository.deleteById(id);
+		
+	}
 }
+
