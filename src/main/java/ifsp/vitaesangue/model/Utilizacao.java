212df47f0d9eb9f8949a56635398b4c72d36b37c
zé <dudd.vitor@gmail.com>
2024-11-24 15:01:10 -0300

    fix: EntityListener in Descarte and Utilizacao

--- src/main/java/ifsp/vitaesangue/model/Utilizacao.java
+++ src/main/java/ifsp/vitaesangue/model/Utilizacao.java
@@ -2,13 +2,16 @@ package ifsp.vitaesangue.model;
 
 import java.time.LocalDateTime;
 
+import ifsp.vitaesangue.listener.HistoricoListener;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
+@EntityListeners(HistoricoListener.class) 
 public class Utilizacao {
 	
 	@Id
