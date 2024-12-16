b512b2001baede73bf557d1f84234ea8c24dbd29
zé <dudd.vitor@gmail.com>
2024-12-16 13:12:26 -0300

    feat: add hemocentroidentificacaoresponse

---
+++ src/main/java/ifsp/vitaesangue/records/hemocentro/HemocentroIdentificacaoResponse.java
@@ -0,0 +1,30 @@
+package ifsp.vitaesangue.records.hemocentro;
+
+public class HemocentroIdentificacaoResponse {
+	private Long id;
+	private String nome;
+	
+	public HemocentroIdentificacaoResponse(Long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public HemocentroIdentificacaoResponse() {
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
